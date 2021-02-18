a7f7bf28dbe9bfceba1af8a259383e398a942ad0
s-weigand <s.weigand.phy@gmail.com>
2021-02-18 19:57:59 +0100

    Added readme and minimal distribution

---
+++ git_install_test_distribution.py
@@ -0,0 +1,4 @@
+"""Minimum Module with __version__"""
+# This version differs on propose to the one in setup.py
+# and demonstrates a version sync problem
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="git_install_test_distribution",
+    version="0.0.2",
+    py_modules=[
+        "git_install_test_distribution",
+    ],
+)
